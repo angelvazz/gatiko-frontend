082bc0ff395181f8b4ed555687d5d518efa6a2ff
Angel Vazquez <angel.mvazz94@gmail.com>
2023-07-28 14:14:25 -0600

    free api

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -9,7 +9,7 @@ const awsconfig = {
   aws_appsync_graphqlEndpoint:
     'https://3o3lxzsr7jeo5cqsaf7fyvz7ce.appsync-api.us-east-1.amazonaws.com/graphql',
   aws_appsync_region: 'us-east-1',
-  aws_appsync_authenticationType: 'AWS_IAM',
+  aws_appsync_authenticationType: 'API_KEY',
   aws_appsync_apiKey: 'da2-t4mywr7avjfkdmqixt25su4lni',
 };
 
